fc73cfb3140f15777a6a87540a4543027386c6b1
Hunain <codife@Hunains-MacBook-Pro.local>
2023-07-30 06:20:53 -0700

    added vercel enc

--- utils/services/apiService.ts
+++ utils/services/apiService.ts
@@ -1,9 +1,10 @@
 import { Data, ErrorResponse } from "@/constants/globaltypes";
 import axios from "axios";
 
-const API_BASE_URL = process.env.PRODUCTION
-  ? `https://assessment-nextjs-page-router.vercel.app`
-  : "http://localhost:3000/api";
+const API_BASE_URL =
+  process.env.ENVIRONMENT === "PRODUCTION"
+    ? `https://assessment-nextjs-page-router.vercel.app`
+    : "http://localhost:3000/api";
 
 export const fetchPage = async (pageNumber: number) => {
   const response = await axios.get<Data>(
